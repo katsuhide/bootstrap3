dd627d7f1bfff3b1fe1afcb1559549ea4e102368
katsuhide <kamonegi.gg@gmail.com>
2014-03-02 21:13:24 +0900

    コミットメント作成時にユーザーIDをひもづけるように対応

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -25,6 +25,7 @@ class CommitmentsController < ApplicationController
   # POST /commitments.json
   def create
     @commitment = Commitment.new(commitment_params)
+    @commitment.user_id = current_user.id
 
     respond_to do |format|
       if @commitment.save
