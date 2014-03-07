55210401310e22ed1df3e00d523408eaa8babb8d
katsuhide <kamonegi.gg@gmail.com>
2014-03-08 01:42:10 +0900

    日付で切り替えるように

--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -1,4 +1,4 @@
 class Commitment < ActiveRecord::Base
-	scope :search_by_user_id, -> (current_user_id){where(:user_id => current_user_id)}
-	scope :search_by_date, -> (search_date) {where(:due_date => search_date)}
+	scope :search_by_user_id, -> (current_user_id){where(:user_id => current_user_id).order("id desc")}
+	scope :search_by_date, -> (search_date) {where(:due_date => search_date).order("id desc")}
 end
